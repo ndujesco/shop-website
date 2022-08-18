af1152168c7881332cd85267759e522f897a66dc
ugo <92054718+ndujesco@users.noreply.github.com>
2022-08-18 11:07:52 +0100

    edited the async

--- util/file.js
+++ util/file.js
@@ -1,11 +1,15 @@
 const fs = require("fs");
 
-const deleteFile = async((filePath) => {
-  fs.unlink(filePath, (err) => {
-    if (err) {
-      throw err;
-    }
-  });
-});
+const deleteFile = async (filePath) => {
+  try {
+    fs.unlink(filePath, (err) => {
+      if (err) {
+        throw err;
+      }
+    });
+  } catch (err) {
+    throw new Error(err);
+  }
+};
 
 exports.deleteFile = deleteFile;
